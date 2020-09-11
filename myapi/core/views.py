341076fc722c622dd7b28face779990db4cc8b60
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2020-09-11 15:46:05 +0100

    Authentication

--- myapi/core/views.py
+++ myapi/core/views.py
@@ -3,11 +3,14 @@ from django.http import JsonResponse
 
 #third party imports
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from . serializers import PostSerializer
 from . models import Post
 
 class TestView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self,request, *args, **kwargs):
         qs = Post.objects.all()
         post = qs.first()
--- myapi/myapi/urls.py
+++ myapi/myapi/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.authtoken.views import obtain_auth_token
 from core.views import TestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',TestView.as_view(),name='test'),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token',obtain_auth_token,name='obtain-token'),
+    path('rest-auth/',include('rest_auth.urls')),
 ]
